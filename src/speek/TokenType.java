659ee1e35f372ef1a342c893bd32e2099d278e45
kumar-vishal01git config --global user.name kumar-vishal01 <vishalk0273@gmail.com>
2026-03-25 23:41:39 +0530

    TokenType

---
+++ src/speek/TokenType.java
@@ -0,0 +1,30 @@
+package speek;
+
+public enum TokenType {
+    LET,
+    BE,
+    SAY,
+    IF,
+    IS,
+    GREATER,
+    THAN,
+    THEN,
+    REPEAT,
+    TIMES,
+
+    NUMBER,
+    STRING,
+    IDENTIFIER,
+
+    PLUS,
+    MINUS,
+    STAR,
+    SLASH,
+
+    GREATER_THAN,
+    LESS_THAN,
+    EQUALS_EQUALS,
+
+    NEWLINE,
+    EOF
+}

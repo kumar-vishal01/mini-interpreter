e1c03c38e85682e787f764786f4460585abf22cc
kumar-vishal01git config --global user.name kumar-vishal01 <vishalk0273@gmail.com>
2026-03-28 00:02:03 +0530

    new Tokenizer class created

---
+++ src/speek/Tokenizer.java
@@ -0,0 +1,20 @@
+package speek;
+
+// This is the final Tokenizer class that will be used to convert the input source code into Tokens.
+
+// First build skeleton 
+public class Tokenizer {
+    private final String source;
+    private int pos;
+    private int line;
+
+    public Tokenizer(String source) {
+        this.source = source;
+        this.pos = 0;
+        this.line = 1;
+    }
+
+    // Now we will loop on the source 
+
+    
+}

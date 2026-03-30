37625d1c6da5645801d22e0914bb450aeee1525a
Vishal Kumar <vishalk0273@gmail.com>
2026-03-30 22:56:42 +0530

    Add tokenize method for token extraction
    Implemented a tokenize method to process the source string and extract tokens. Added support for reading words and recognizing keywords.

--- src/speek/Tokenizer.java
+++ src/speek/Tokenizer.java
@@ -16,6 +16,62 @@ public class Tokenizer {
 
     // Now we will run loop on the source 
     // Do further things
+    public List<Token> tokenize() {
+        List<Token> tokens = new ArrayList<>();
+
+        while ( pos < source.length()){
+            char c = source.charAt(pos);
+
+            if( c == ' ' || c == '\t' || c == '\r'){
+                pos++;
+                continue;
+            }
+
+            if (Character.isLetter(c) || c == '_'){
+                tokens.add(readWord());
+                continue;
+            }
+
+          pos++;  
+            
+        }
+        return tokens;
+    }
+
+
+    private Token readWord(){
+        int startLine = line;
+        StringBuilder sb = new StringBuilder();
+
+        while (pos < source.length() && (Character.isLetterOrDigit(source.charAt(pos)) ||source.charAt(pos) == '_')){
+            sb.append(source.charAt(pos));
+            pos++;
+        }
+
+        String word = sb.toString();
+        TokenType type = keyword(word);
+
+        return new Token(type, word, startLine);
+    }
+
+
+
+    private TokenType keyword(String word){
+        switch (word) {
+            case "let":     return TokenType.LET;
+            case "be":      return TokenType.BE;
+            case "say":     return TokenType.SAY;
+            case "if":      return TokenType.IF;
+            case "is":      return TokenType.IS;
+            case "greater": return TokenType.GREATER;
+            case "than":    return TokenType.THAN;
+            case "then":    return TokenType.THEN;
+            case "repeat":  return TokenType.REPEAT;
+            case "times":   return TokenType.TIMES;
+            default:        return TokenType.IDENTIFIER;
+        }
+    }
+
 
     
 }

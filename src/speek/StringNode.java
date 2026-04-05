ca66051e4b0126dfa86e5f2ad7ebd28557809339
Neeraj Parmar <neerajparmar748@gmail.com>
2026-04-05 11:39:10 +0530

    define StringNode class

--- src/speek/StringNode.java
+++ src/speek/StringNode.java
@@ -2,6 +2,16 @@ package speek;
 
 
 public class StringNode implements Expression {
+    private final String value;
+
+    public StringNode(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public Object evaluate(Environment env) {
+        return value;
+    }
     
 
 }

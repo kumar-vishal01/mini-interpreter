9a1c6c5860e7cdf291e991202fb1a8ced8828e6f
Shahid Ansari <heyshahid786@gmail.com>
2026-04-14 00:14:24 +0530

    Added the interpreter file

---
+++ src/speek/Interpreter.java
@@ -0,0 +1,22 @@
+package speek;
+
+import java.util.List;
+
+public class Interpreter {
+
+    public void run(String sourceCode) {
+        // Step 1: Tokenize
+        Tokenizer tokenizer = new Tokenizer(sourceCode);
+        List<Token> tokens = tokenizer.tokenize();
+
+        // Step 2: Parse
+        Parser parser = new Parser(tokens);
+        List<Instruction> instructions = parser.parse();
+
+        // Step 3: Execute
+        Environment env = new Environment();
+        for (Instruction instr : instructions) {
+            instr.execute(env);
+        }
+    }
+}
